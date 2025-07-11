d590d8c3025074a963fd0b86a4e4cdf092f4b2ce
haeuniiii <whgkdms020506@gmail.com>
2025-07-12 03:08:13 +0900

    init: Initial Waitil Spring Project

---
+++ src/main/java/com/waitil/wait_backend/WaitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.waitil.wait_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waitil/wait_backend/WaitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waitil.wait_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
