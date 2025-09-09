5fd153773e82a41637560c2d8ca31b319dc41e18
조하은 <whgkdms020506@gmail.com>
2025-09-09 10:10:45 +0900

    주석확인

--- src/test/java/com/waitil/wait_backend/WaitBackendApplicationTests.java
+++ src/test/java/com/waitil/wait_backend/WaitBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.waitil.wait_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest //커밋확인
 class WaitBackendApplicationTests {
 
 	@Test
